4a5c363397a9bc8ec29dae58792284180c4bfab9
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-02-27 17:30:43 -0300

    Implementação do processo para lidar com .txt

---
+++ ReadTxt/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using ReadTxt.Models;
+
+namespace ReadTxt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            List<Customer> customers = new List<Customer>();
+            string filePath = string.Empty;
+            if (postedFile != null)
+            {
+                string path = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                filePath = path + Path.GetFileName(postedFile.FileName);
+                string extension = Path.GetExtension(postedFile.FileName);
+                postedFile.SaveAs(filePath);
+
+                //Read the contents of CSV file.
+                string csvData = System.IO.File.ReadAllText(filePath);
+
+                //Execute a loop over the rows.
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        customers.Add(new Customer
+                        {
+                            CustomerId = Convert.ToInt32(row.Split(',')[0]),
+                            Name = row.Split(',')[1],
+                            Country = row.Split(',')[2]
+                        });
+                    }
+                }
+            }
+
+            return Ok(customers);
+        }
+    }
+}
