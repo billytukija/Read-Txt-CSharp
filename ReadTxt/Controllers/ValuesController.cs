451126e0b150b2598007b24479b25758e28400b5
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-02-28 16:11:59 -0300

    Lidando com .txt

--- ReadTxt/Controllers/ValuesController.cs
+++ ReadTxt/Controllers/ValuesController.cs
@@ -11,40 +11,11 @@ namespace ReadTxt.Controllers
     public class ValuesController : ControllerBase
     {
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase postedFile)
+        public ActionResult Index(HttpPostedFileWrapper inputArquivo)
         {
             List<Customer> customers = new List<Customer>();
-            string filePath = string.Empty;
-            if (postedFile != null)
-            {
-                string path = Server.MapPath("~/Uploads/");
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
 
-                filePath = path + Path.GetFileName(postedFile.FileName);
-                string extension = Path.GetExtension(postedFile.FileName);
-                postedFile.SaveAs(filePath);
-
-                //Read the contents of CSV file.
-                string csvData = System.IO.File.ReadAllText(filePath);
-
-                //Execute a loop over the rows.
-                foreach (string row in csvData.Split('\n'))
-                {
-                    if (!string.IsNullOrEmpty(row))
-                    {
-                        customers.Add(new Customer
-                        {
-                            CustomerId = Convert.ToInt32(row.Split(',')[0]),
-                            Name = row.Split(',')[1],
-                            Country = row.Split(',')[2]
-                        });
-                    }
-                }
-            }
-
+            var lines = Services.FilesServices.GetFileLines(inputArquivo);
             return Ok(customers);
         }
     }
---
+++ ReadTxt/Services/FilesServices.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace ReadTxt.Services
+{
+    public static class FilesServices
+    {
+        public static List<string> GetFileLines(Stream inputArquivo)
+        {
+            var FileLines = new List<string>();
+
+            using (StreamReader leitor = new StreamReader(inputArquivo))
+            {
+                do
+                {
+                    var next = leitor.ReadLine();
+                    FileLines.Add(next);
+                } while (leitor.Peek() != -1);
+            }
+
+            return FileLines;
+        }
+    }
+}
